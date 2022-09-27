df627d468008e1ebaf3a416f435e4a7128487653
ragulJeevan <raguljeevan08@gmail.com>
2022-09-27 11:40:13 +0530

    repeative bug cleared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'whitehouseTask';
-  row:any = [
+  rowC:any = [
+    {
+      item1: '',
+      item2: '',
+      item3: ''
+    }
+  ];
+  rowB:any = [
+    {
+      item1: '',
+      item2: '',
+      item3: ''
+    }
+  ];
+  rowK:any = [
+    {
+      item1: '',
+      item2: '',
+      item3: ''
+    }
+  ];
+  rowM:any = [
     {
       item1: '',
       item2: '',
@@ -16,30 +37,30 @@ export class AppComponent implements OnInit{
   ];
   constructor(){}
   ngOnInit(): void {}
-  addTable() {
+  addTable(item:any) {
     const obj = {
       item1: '',
       item2: '',
       item3: ''
     }
-    this.row.push(obj)
+    item.push(obj);
   }
   deleteRow(x:any){
-    this.row.splice(x, 1 );
+    this.rowC.splice(x, 1 );
   }
-  item1Total(){
-   return this.row.map((a:any)=>{return a.item1}).reduce((a:any,b:any)=>{
+  item1Total(item:any){
+   return item.map((a:any)=>{return a.item1}).reduce((a:any,b:any)=>{
     return b+a;
    }); 
 
   }
-  item2Total(){
-    return this.row.map((a:any)=>{return a.item2}).reduce((a:any,b:any)=>{
+  item2Total(item:any){
+    return item.map((a:any)=>{return a.item2}).reduce((a:any,b:any)=>{
      return b+a;
     });  
    }
-   item3Total(){
-    return this.row.map((a:any)=>{return a.item3}).reduce((a:any,b:any)=>{
+   item3Total(item:any){
+    return item.map((a:any)=>{return a.item3}).reduce((a:any,b:any)=>{
      return b+a;
     });  
    }
